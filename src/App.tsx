779ef780d79e283afd8fbf8f97827424a50f48eb
Mostafa Rasouli <Kasraco@gmail.com>
2024-10-03 17:34:19 +0330

    Added font awesome to my project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
+import Register from "./Register";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <Register />;
 }
 
 export default App;
-
