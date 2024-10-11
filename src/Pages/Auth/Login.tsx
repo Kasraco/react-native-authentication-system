efe3677dea85f5cb359206a54057d12f74d10eb3
Mostafa Rasouli <Kasraco@gmail.com>
2024-10-11 15:35:38 +0330

    Updated setAuth to exclude password from authentication state

--- src/Pages/Auth/Login.tsx
+++ src/Pages/Auth/Login.tsx
@@ -35,7 +35,7 @@ const Login = () => {
 
       const accessToken = response?.data?.accessToken;
       const roles = response?.data?.roles;
-      setAuth({ user, pwd, roles, accessToken });
+      setAuth({ user, roles, accessToken });
       setUser("");
       setPwd("");
 
