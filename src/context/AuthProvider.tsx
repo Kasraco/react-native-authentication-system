715803a9d607f2323b7f49d0ea867a7f540eb1eb
Mostafa Rasouli <Kasraco@gmail.com>
2024-10-11 15:36:13 +0330

    Enhanced AuthProvider with TypeScript types for improved type safety

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,11 +1,27 @@
 import { createContext, useState } from "react";
+import { AuthContextType, AuthData, AuthProvideProps } from "../types";
 
-const AuthContext = createContext({});
+// const AuthContext = createContext({});
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export const AuthProvider = ({ children }) => {
-  const [Auth, setAuth] = useState({});
+export const AuthProvider: React.FC<AuthProvideProps> = ({ children }) => {
+  const [Auth, setAuth] = useState<AuthData>({});
 
   return <AuthContext.Provider value={{ Auth, setAuth }}>{children}</AuthContext.Provider>;
 };
 
 export default AuthContext;
+
+//Older Code
+
+// import { createContext, useState } from "react";
+// import { AuthContextType } from "../types";
+
+// const AuthContext = createContext({});
+
+// export const AuthProvider = ({ children }) => {
+//   const [Auth, setAuth] = useStat({});
+
+//   return <AuthContext.Provider value={{ Auth, setAuth }}>{children}</AuthContext.Provider>;
+// };
+// export default AuthContext;
