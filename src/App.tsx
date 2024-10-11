b3515c969c67497dbc06879c61bb281f72bf8fbb
Mostafa Rasouli <Kasraco@gmail.com>
2024-10-11 14:49:25 +0330

    Added React Router for page navigation and implemented routes for Login, Register, Home, and 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./Login";
-import Register from "./Register";
+import Login from "./Pages/Auth/Login";
+import Register from "./Pages/Auth/Register";
+import { Layout } from "./Pages/Layout";
+import { Missing } from "./Pages/Missing";
+import { Home } from "./Pages/Home";
 
 function App() {
   return (
-    <main className="App">
-      {/* <Register /> */}
-      <Login />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* hameye componenthaye zir dara layout render misha vand */}
+        <Route path="Login" element={<Login />} />
+        <Route path="Register" element={<Register />} />
+
+        <Route index element={<Home />} />
+
+        {/* in code kare safheye 404 ra mikonad */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
